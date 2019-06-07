4b3bdf3ddf2cc807fe8a69cc1ebafde0b89144e3
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-06-07 12:04:58 -0600

    [NF] Get decoder for row

--- Sources/SQL/Coding/Row+Decoding.swift
+++ Sources/SQL/Coding/Row+Decoding.swift
@@ -10,18 +10,24 @@ import Swiftlier
 
 extension Row {
     public func decode<T: TableStorable>(purpose: CodingPurpose, userInfo: [CodingUserInfoKey:Any] = [:]) throws -> T where T: Decodable {
-        let decoder = RowDecoder(row: self, forTableNamed: T.tableName)
-        decoder.userInfo = userInfo
-        decoder.userInfo.location = .local
-        decoder.userInfo.purpose = purpose
+        let decoder = self.decoder(purpose: purpose, userInfo: userInfo, tableName: T.tableName)
         return try T(from: decoder)
     }
 
     public func decodeRaw<D: Decodable>(purpose: CodingPurpose, userInfo: [CodingUserInfoKey:Any] = [:], tableName: String? = nil) throws -> D {
+        let decoder = self.decoder(purpose: purpose, userInfo: userInfo, tableName: tableName)
+        return try D(from: decoder)
+    }
+
+    public func decoder<T: TableStorable>(purpose: CodingPurpose, userInfo: [CodingUserInfoKey:Any] = [:], for type: T.Type) -> Decoder {
+        return self.decoder(purpose: purpose, userInfo: userInfo, tableName: T.tableName)
+    }
+
+    public func decoder(purpose: CodingPurpose, userInfo: [CodingUserInfoKey:Any] = [:], tableName: String? = nil) -> Decoder {
         let decoder = RowDecoder(row: self, forTableNamed: tableName)
         decoder.userInfo = userInfo
         decoder.userInfo.location = .local
         decoder.userInfo.purpose = purpose
-        return try D(from: decoder)
+        return decoder
     }
 }
